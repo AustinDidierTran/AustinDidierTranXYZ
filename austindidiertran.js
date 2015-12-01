f20799b4f16e020fedb089f4af437eab8768ae71
austindidiertran1 <tran.austin.didier@gmail.com>
2015-12-01 00:03:27 +0000

    Created experience-professionnelle element, now really easy to add experience

--- austindidiertran.js
+++ austindidiertran.js
@@ -1,14 +1,26 @@
-$( document ).ready(function(){
-    
+$(document).ready(function() {
+
     init();
 
 });
 
 var init = function() {
-        document.addEventListener('WebComponentsReady', function() {
-                var template = document.querySelector('template[is="dom-bind"]');
+    document.addEventListener('WebComponentsReady', function() {
+        var template = document.querySelector('template[is="dom-bind"]');
 
-                template.selected = 0;
-            })
-        }
-        
+        template.selected = 0;
+
+        template.jobs = [{
+            "img": "../pictures/Ubisoft.png",
+            "company": "Ubisoft Divertissements",
+            "title": "Programmeur outils - Stagiaire",
+            "timespan": "Hiver 2016"
+        },{
+            "img": "../pictures/Tyco.png",
+            "company": "Tyco Safety Products",
+            "title": "Concepteur logiciel - Stagiaire",
+            "timespan": "Été 2015"
+        }];
+
+    })
+}
